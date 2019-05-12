d2e39f2e96c34bc8a1c2410a4ac60af4719dbf6c
Michał Dudek <dudek.michal1996@gmail.com>
2019-05-12 17:53:41 +0200

    reduxdevtools remove

--- src/store.js
+++ src/store.js
@@ -3,16 +3,10 @@ import reduxThunk from "redux-thunk";
 
 import reducers from "./redux/reducers";
 
-const ReactReduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 export default createStore(
   reducers,
   {
     authReducer: { authenticated: localStorage.getItem("token") }
   },
-  compose(
-    applyMiddleware(reduxThunk),
-    ReactReduxDevTools
-  )
+  compose(applyMiddleware(reduxThunk))
 );
